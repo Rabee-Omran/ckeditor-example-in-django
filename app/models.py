df6d26e64d250900baa8da90c672b7e52c7bdb0c
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-05-12 18:58:18 +0300

    create app and model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
+class Post(models.Model):
+    title          = models.CharField( max_length=50)
+    description    = RichTextField()
